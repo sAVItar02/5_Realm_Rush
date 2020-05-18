c4cb0ce53fcad5688a0b8c74571545fb93ad7617
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-05-18 19:48:44 +0530

    Initial commit

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * 10f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * 10f;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
