53fdf56c2cb858f7eab0334ec1564604d90a8e16
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-05-18 19:51:30 +0530

    Editor GridSnap script complete Cube prefab made

--- Assets/Scripts/EditorSnap.cs
+++ Assets/Scripts/EditorSnap.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class EditorSnap : MonoBehaviour
 {
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
     // Update is called once per frame
     void Update()
     {
         Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * 10f;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * 10f;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
     }
