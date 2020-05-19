586bb7a29aa42cb1ac3859064f7a261a4b4c87e6
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-05-19 14:41:45 +0530

    Dictionary to store waypoints and check duplication complete

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            if (grid.ContainsKey(waypoint.GetGridPos()))
+            {
+                Debug.LogWarning("Skipping Block " + waypoint);
+            }
+            else
+            {
+                grid.Add(waypoint.GetGridPos(), waypoint);
+            }
+        }
+        //print("Added: " + grid.Count + " Waypoints");
+    }
+}
