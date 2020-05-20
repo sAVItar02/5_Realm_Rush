620d47ecc5d91d85965383f41cf55394abb79d9a
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-05-20 16:44:39 +0530

    cubes exchanged to prefabs , Enemy damage script written
    Enemy box colliders and tower Movement scripts written

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int enemyHitPoints = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if(enemyHitPoints < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void ProcessHit()
+    {
+        enemyHitPoints--;
+        print("Enemy Heath = " + enemyHitPoints);
+    }
+}
