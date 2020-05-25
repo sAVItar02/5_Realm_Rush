910eec322b6c3cdb45ad03467e74f35f962e8a76
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-05-25 14:23:02 +0530

    Health Bar Implemented
    Player Health script changes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    int playerHealth = 10;
-    int damgeOnHit = 1;
+    [SerializeField] int playerHealth = 10;
+    [SerializeField] int damgeOnHit = 1;
+    [SerializeField] Text healthText;
+
+    public HealthBar healthBar;
+
+    private void Start()
+    {
+        healthBar.SetMaxHealth(playerHealth);
+        //healthText.text = playerHealth.ToString();
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        playerHealth = playerHealth - damgeOnHit;
-        Debug.Log(playerHealth);
+        playerHealth -= damgeOnHit;
+        healthBar.SetHealth(playerHealth);
+        //healthText.text = playerHealth.ToString();
     }
 }
